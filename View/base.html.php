613c908a085289727d02bd4f369e07c69bde3b9f
Wakelam jordan <jwakelams@gmail.com>
2024-01-31 11:07:35 +0100

    mep du bs et du fa

--- View/base.html.php
+++ View/base.html.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="./Public/css/style.css">
+    <link rel="stylesheet" href="./Public/style/fontawesome-free-6.5.1-web/css/all.css">
+    <link rel="stylesheet" href="./Public/style/bootstrap-5.3.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" href="./Public/style/style.css">
+    <script src="../Public/style/bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js" defer></script>
 </head>
 
-<body>
+<body class="">
     
 
-<header>
+<header class="">
     <p>Livraison GRATUITE dans toute L'EUROPE</p>
     
 </header>
@@ -18,9 +21,9 @@
 <main>
     <h1>E-success</h1>
     <nav>
-        <ul>
+        <ul class="">
             <li><a href="acceuil">ACCEUIL</a></li>
-            <li><a href="#">PRODUIT</a></li>
+            <li><a href="#" class="">PRODUIT</a></li>
             <li><a href="#">MON COMPTE</a></li>
             <li><a href="#">PARAMETRE</a>
             <ul><a href="#"></a>USER</ul>
