50b3521ab060544a5cab47082902f3b70913acc2
Wakelam jordan <jwakelams@gmail.com>
2024-02-01 10:43:32 +0100

    maj role

--- conception/bdd/sql2.sql
+++ conception/bdd/sql2.sql
@@ -484,7 +484,7 @@ VALUES
 /* user */
 INSERT INTO `e_success`.`user` (`login`, `mail`, `password`, `roles`, `date_create`, `id_people`)
 VALUES
-    ('admin', 'john.doe@example.com', '6e1b1f20acc26e074ef1f250f0b3f1ba27c1c29d', '["ROLE_USER","ADMIN_ROLE"]', NOW(), 1),
+    ('admin', 'john.doe@example.com', '6e1b1f20acc26e074ef1f250f0b3f1ba27c1c29d', '["ROLE_USER","ROLE_ADMIN"]', NOW(), 1),
     ('caisse', 'alice.smith@example.com', '6e1b1f20acc26e074ef1f250f0b3f1ba27c1c29d', '["ROLE_USER","CAISSE_ROLE"]', NOW(), 2),
     ('sav', 'bob.johnson@example.com', '6e1b1f20acc26e074ef1f250f0b3f1ba27c1c29d', '["ROLE_USER","SAV_ROLE"]', NOW(), 3),
     ('eva_martinez', 'eva.martinez@example.com', '6e1b1f20acc26e074ef1f250f0b3f1ba27c1c29d', '["ROLE_USER"]', NOW(), 4),
